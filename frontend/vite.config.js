eca37166231de6025e741fdeab6551a855365a53
Lorenzo Diel <lorenzodiel5@gmail.com>
2026-01-08 19:34:06 -0300

    Ajuste error CORS 2.1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   preview: {
     allowedHosts: ["adaptable-balance-production.up.railway.app"],
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-      },
-    },
-  },
-})
+  server:
+    command === "serve"
+      ? {
+          proxy: {
+            "/api": {
+              target: "http://127.0.0.1:8000",
+              changeOrigin: true,
+            },
+          },
+        }
+      : undefined,
+}));
